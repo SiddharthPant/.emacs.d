a16e2c8afb6298a29d6962db9094f93320626ac5
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-08-22 23:41:13 +0530

    Add melpa repo, naysayer-theme

--- init.el
+++ init.el
@@ -1,16 +1,23 @@
-(setq make-backup-files nil) ; stop creating ~ files
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+(setq make-backup-files nil)
 (setq select-enable-clipboard t)
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(load-theme 'wombat)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(use-package naysayer-theme
+  :ensure t
+  :config
+  (load-theme 'naysayer t))
 (set-face-attribute 'default nil
-		    :font "Iosevka"
-		    :height 130) ; 12pt font
+		    :font "Consolas"
+		    :height 110) ; 11pt font
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
 (use-package rust-mode :ensure t)
 (fido-vertical-mode 1)
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status))
+(use-package magit :ensure t)
